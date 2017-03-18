cd41c30b5b6860a4838470c0100eae1e9cd21b3a
Nazim Hajidin <nhajidin@gmail.com>
2017-03-18 10:55:09 -0400

    Add scanner code

--- App.js
+++ App.js
@@ -1,14 +1,76 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, ListView, Alert } from 'react-native';
+import Expo, { Components, Permissions } from 'expo';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+    this.state = {
+      data: [],
+      dataSource: ds.cloneWithRows([]),
+      hasCameraPermission: null,
+      scannerOn: false,
+    };
+  }
+
+  async componentWillMount() {
+    const { status } = await Permissions.askAsync(Permissions.CAMERA);
+    this.setState({hasCameraPermission: status === 'granted'});
+  }
+
+  openScanner = () => {
+    if (this.state.hasCameraPermission) {
+      this.setState(prev => ({ scannerOn: true }));
+    } else {
+      Alert.alert('No access to camera');
+    }
+  }
+
+  handleScanner = scannerData => {
+    this.setState(prev => {
+      const data = prev.data.concat(scannerData.data);
+
+      Alert.alert(JSON.stringify(Components.BarCodeScanner.BarCodeTypes));
+
+      return {
+        data,
+        dataSource: prev.dataSource.cloneWithRows(data),
+        scannerOn: false,
+      }
+    });
+  }
+
   render() {
+    let contents;
+    if (this.state.hasCameraPermission && this.state.scannerOn) {
+      contents = (
+        <Components.BarCodeScanner
+          onBarCodeRead={this.handleScanner}
+          style={StyleSheet.absoluteFill}
+          barCodeTypes={['ean13']}
+        />
+      );
+    } else {
+      contents = (
+        <View style={{ marginTop: 50 }}>
+          <Button
+            title="Scan"
+            onPress={this.openScanner}
+          />
+          <ListView
+            dataSource={this.state.dataSource}
+            renderRow={(rowData) => <Text>{rowData}</Text>}
+            enableEmptySections
+          />
+        </View>
+      );
+    }
+
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
+    <View style={styles.container}>
+      {contents}
+    </View>
     );
   }
 }
@@ -18,6 +80,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
   },
 });
